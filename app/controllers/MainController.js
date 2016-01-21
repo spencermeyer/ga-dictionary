fec408bcd885aa233010a77289bac0216da89ea3
alexpchin <alex@alexchin.co.uk>
2016-01-21 16:58:35 +0000

    Refactoring SCSS

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -4,18 +4,7 @@ module.exports = function() {
 	var self      = this;
 	self.query    = "";
 	self.all      = dictionary;
-	self.letters  = 'abcdefghijklmnopqrstuvwxyz'.split('');
-	self.selected = {};
-
-	self.sort = function(query) {
-		event.preventDefault();
-		self.query = query;
-	};
-
-	self.define = function(item) {
-		event.preventDefault();
-		self.selected = item;
-	};
+	self.letters  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
 	self.open = function(item){
 		$('#definition').modal('show');
